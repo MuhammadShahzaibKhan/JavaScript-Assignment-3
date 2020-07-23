d6d0d2aa5150c26ad0d078f08f68f9e47fb2c894
Muhammad Shahzaib Khan <mshahzaibkhan17@gmail.com>
2020-07-23 23:36:25 +0500

    JavaScript Assignment 3 ready to commit

---
+++ app.js
@@ -0,0 +1,145 @@
+/// ===== Assignment 43.1 =====///
+
+// function outline(){
+//     var imgBorder = document.getElementById("mob");
+//     imgBorder.className = "hidden";
+// }
+
+// function outline1(){
+//     var imgBorder = document.getElementById("mob1")
+//     imgBorder.className = "hidden";
+//     alert("Thank you for purchasing a phone from us");
+// }
+
+// function outline2(){
+//     var imgBorder = document.getElementById("mob2")
+//     imgBorder.className = "hidden";
+// }
+
+// function outline3(){
+//     var imgBorder = document.getElementById("mob3")
+//     imgBorder.className = "hidden";
+// }
+
+/// =====  =====///
+
+
+/// ===== Assignment 43.2 =====///
+
+// function outline(){
+//     var imgBorder = document.getElementById("mob");
+//     imgBorder.className = "hidden";
+//     alert("Thank you for purchasing a phone from us");
+// }
+
+// function outline1(){
+//     var imgBorder = document.getElementById("mob1")
+//     imgBorder.className = "hidden";
+//     alert("Thank you for purchasing a phone from us");
+// }
+
+// function outline2(){
+//     var imgBorder = document.getElementById("mob2")
+//     imgBorder.className = "hidden";
+//     alert("Thank you for purchasing a phone from us");
+// }
+
+// function outline3(){
+//     var imgBorder = document.getElementById("mob3")
+//     imgBorder.className = "hidden";
+//     alert("Thank you for purchasing a phone from us");
+// }
+
+
+/// =====  =====///
+
+
+
+/// ===== Assignment 43.3 =====///
+
+
+
+
+/// =====  =====///
+
+
+/// ===== Assignment 43.4 =====///
+
+
+
+// function changePicture(){
+//     var image = document.getElementById("car");
+//     image.src = "Pics/car image2.webp"
+// }
+
+// function changePictureBack(){
+//     var image = document.getElementById("car");
+//     image.src = "Pics/car image1.webp"
+// }
+
+
+
+
+/// =====  =====///
+
+
+/// ===== Assignment 43.5 =====///
+
+
+// function addNumber(){
+//     var add = document.getElementById("number").innerHTML
+//     add++;
+//     document.getElementById("number").innerHTML = add;
+// }
+
+// function subtractNumber(){
+//     var subtract = document.getElementById("number").innerHTML
+//     subtract--;
+//     document.getElementById("number").innerHTML = subtract;
+// }
+
+
+
+/// =====  =====///
+
+
+
+/// ===== Assignment 49.2 =====///
+
+
+// function readMore(){
+//     var para1 = document.getElementById("para1").innerHTML;
+//     var para2 = document.getElementById("paragraph");
+//     para2.innerHTML = para1;
+// }
+
+
+/// =====  =====///
+
+
+
+/// ===== Assignment 53.1 =====///
+
+
+
+// function bigImage(e){
+//     var modalImage = document.getElementById("modalImage");
+//     modalImage.src = e.src
+// }
+
+
+/// =====  =====///
+
+
+
+/// ===== Assignment 58.1 =====///
+
+///// 58.1.1
+
+// var main = document.childNodes[1].childNodes[2].childNodes[43].childNodes[5];
+// console.log(main);
+
+///// 58.1.2
+
+// var main = document.childNodes[1].childNodes[2].childNodes[43].childNodes[5];
+// console.log(main.childNodes);
